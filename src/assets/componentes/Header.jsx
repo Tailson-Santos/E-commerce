627abfbaf42860494b93b5a26dd3fbe60b22920d
Tailson Moraes <tailsonfrontend@gmail.com>
2026-05-20 17:32:22 -0300

    atualizando css header

--- src/assets/componentes/Header.jsx
+++ src/assets/componentes/Header.jsx
@@ -4,7 +4,7 @@ import {Filtro} from '../componentes/Filtro'
 
 export function Header(){
     return(
-        <header className='flex justify-between items-center p-2  bg-amber-300'>
+        <header className='flex justify-between items-center p-2 bg-[var(--color-secondary)]'>
             <div>
                 <img src={logo} alt="logo-img" width={80}/>
             </div>
