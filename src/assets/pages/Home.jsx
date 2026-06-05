dfe83a07de8e528c8b4bc306040d6ee7e89d3910
Tailson Moraes <tailsonfrontend@gmail.com>
2026-06-05 16:36:24 -0300

    removendo componente de carrosel

--- src/assets/pages/Home.jsx
+++ src/assets/pages/Home.jsx
@@ -1,17 +1,16 @@
 import { Header } from "../componentes/Header";
-import { CarrosselPromocao } from "../componentes/CarrosselPromocao";
 import { Conteudo } from "../componentes/Conteudo";
+import {FiltroCategorias} from "../componentes/FiltroCategorias"
+
 
 export function Home() {
   return (
-    <>
+    <main>
+        
       <Header />
-
-      <div className="max-w-7xl mx-auto p-2">
-        <CarrosselPromocao />
-      </div>
-
+      
+      <FiltroCategorias/>
       <Conteudo />
-    </>
+    </main>
   );
 }
